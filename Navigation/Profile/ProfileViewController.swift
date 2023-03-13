b15a78cbc9ab6048fbfe99b61a3b3eed90dbd0f9
serjchaika <105229037+serjchaika@users.noreply.github.com>
2023-03-13 15:12:52 +0300

    2.2. Вёрстка экранов для iOS. Auto Layout. Полностью

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -3,47 +3,60 @@ import UIKit
 final class ProfileViewController: UIViewController {
 
     private lazy var profileView: ProfileHeaderView = {
-        let view = ProfileHeaderView(frame: CGRect(
-            x: 0,
-            y: 0,
-            width: view.safeAreaLayoutGuide.layoutFrame.size.width,
-            height: view.safeAreaLayoutGuide.layoutFrame.size.height
-        ))
-        return view
-    }()
-
-    private lazy var headerView: UIView = {
-        let view = UIView(frame: CGRect(
-            x: 0,
-            y: 120,
-            width: view.safeAreaLayoutGuide.layoutFrame.size.width,
-            height: view.safeAreaLayoutGuide.layoutFrame.size.height - 120
-        ))
+        let view = ProfileHeaderView()
+        view.translatesAutoresizingMaskIntoConstraints = false
         view.backgroundColor = .systemGray4
         return view
     }()
 
+    private lazy var goButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Go!", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Profile"
+        title = Constants.profileTitle
         view.backgroundColor = .white
-
-        view.addSubview(headerView)
-        headerView.addSubview(profileView)
-
-//        print(Float(tabBarController?.tabBar.frame.size.width ?? 0))
-//        print(Float(tabBarController?.tabBar.frame.size.height ?? 0))
+        view.addSubview(profileView)
+        view.addSubview(goButton)
+        makeUI()
     }
 
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
+    private func makeUI() {
+        let safeAreaLayoutGuide = view.safeAreaLayoutGuide
 
-        headerView.frame = CGRect(
-            x: 0,
-            y: 64,
-            width: view.safeAreaLayoutGuide.layoutFrame.size.width,
-            height: view.safeAreaLayoutGuide.layoutFrame.size.height
-        )
+        NSLayoutConstraint.activate([
+            profileView.leadingAnchor.constraint(
+                equalTo: safeAreaLayoutGuide.leadingAnchor
+            ),
+            profileView.trailingAnchor.constraint(
+                equalTo: safeAreaLayoutGuide.trailingAnchor
+            ),
+            profileView.topAnchor.constraint(
+                equalTo: safeAreaLayoutGuide.topAnchor
+            ),
+            profileView.heightAnchor.constraint(
+                equalToConstant: 220.0
+            ),
+
+            goButton.leadingAnchor.constraint(
+                equalTo: safeAreaLayoutGuide.leadingAnchor,
+                constant: 0.0
+            ),
+            goButton.trailingAnchor.constraint(
+                equalTo: safeAreaLayoutGuide.trailingAnchor,
+                constant: 0.0
+            ),
+            goButton.bottomAnchor.constraint(
+                equalTo: safeAreaLayoutGuide.bottomAnchor
+            ),
+            goButton.heightAnchor.constraint(
+                equalToConstant: 50.0
+            ),
+        ])
     }
-
 }
